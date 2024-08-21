8be8ff40a0936758d2428aaf879cfe6cad18c4ea
nikhilkumar110k <nikhil4040k@gmail.com>
2024-08-21 19:20:26 +0530

    working code for spark

--- spark.py
+++ spark.py
@@ -1,2 +1,16 @@
 from pyspark.sql import SparkSession
 spark=SparkSession.builder.appName("BDA Project").getOrCreate()
+from pyspark.ml.feature import  VectorAssembler
+from pyspark.sql.functions import when
+
+df= spark.read.csv("/content/BDA NIKHIL PROJECT Book1.csv", inferSchema=True, header=True)
+
+df.printSchema()
+df.head(1)
+df= df.na.fill(4, subset=['RAM Specifications'])
+df.show(1300)
+df= df.withColumn("Reviews", when(df["Reviews"] >5.0, df["Reviews"]-4).otherwise(df["Reviews"]))
+df.show(1300)
+
+
+
