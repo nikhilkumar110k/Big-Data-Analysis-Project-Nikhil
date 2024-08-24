ab0ad2db78b0424bcea75ee8102e90cec6ba2b6a
nikhilkumar110k <nikhil4040k@gmail.com>
2024-08-24 18:11:27 +0530

    further preprocessing

--- spark.py
+++ spark.py
@@ -1,16 +1,26 @@
 from pyspark.sql import SparkSession
-spark=SparkSession.builder.appName("BDA Project").getOrCreate()
+from pyspark.sql.functions import when, col, lit
 from pyspark.ml.feature import  VectorAssembler
-from pyspark.sql.functions import when
+from pyspark.sql.types import DoubleType
+from pyspark.sql.functions import regexp_replace
 
-df= spark.read.csv("/content/BDA NIKHIL PROJECT Book1.csv", inferSchema=True, header=True)
 
+df= spark.read.csv("/content/Scrapped_Data_for_allinfo.csv", inferSchema=True, header=True)
+
+df=df.withColumn("Prices",(regexp_replace(col("Prices"), ",", "").cast(DoubleType())))
+df.dropDuplicates(["Mobile Name", "Prices", "Reviews", "RAM Specifications", "Storage Specifications"])
 df.printSchema()
 df.head(1)
+
 df= df.na.fill(4, subset=['RAM Specifications'])
 df.show(1300)
 df= df.withColumn("Reviews", when(df["Reviews"] >5.0, df["Reviews"]-4).otherwise(df["Reviews"]))
 df.show(1300)
 
 
+
+df0_10000 = df.withColumn("Prices", when(col("Prices") <= 10000, col("Prices")).otherwise(lit(None).cast(df.schema["Prices"].dataType)))
+df0_10000= df.na.drop(subset=['Prices'])
+df0_10000.dropDuplicates(["Mobile Name", "Prices", "Reviews", "RAM Specifications", "Storage Specifications"])
+df0_10000.count()
 
