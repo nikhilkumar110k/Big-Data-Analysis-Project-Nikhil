144cd3b23ab765d7d3576078ea63188548ed663d
nikhilkumar110k <nikhil4040k@gmail.com>
2024-08-29 21:03:01 +0530

    updated code

--- spark.py
+++ spark.py
@@ -3,7 +3,9 @@ from pyspark.sql.functions import when, col, lit
 from pyspark.ml.feature import  VectorAssembler
 from pyspark.sql.types import DoubleType
 from pyspark.sql.functions import regexp_replace
-
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.feature import VectorAssembler
 
 df= spark.read.csv("/content/Scrapped_Data_for_allinfo.csv", inferSchema=True, header=True)
 
@@ -34,3 +36,18 @@ train_datafrom0_10000, test_datafrom0_10000= df0_10000.randomSplit([0.7,0.3])
 
 train_datafrom0_10000.describe().show()
 test_datafrom0_10000.describe().show()
+
+assembler= VectorAssembler(inputCols=['Prices','Reviews','RAM Specifications','Storage Specifications'], outputCol='features', handleInvalid='skip')
+output= assembler.transform(df)
+fnl_data=output.select('features','Reviews')
+fnl_data.show()
+train_fnl_data, test_fnl_data= fnl_data.randomSplit([0.7,0.3])
+fnl_data.na.drop()
+fnl_data.count()
+lr= LinearRegression(labelCol='Reviews')
+
+lr_model=lr.fit(train_fnl_data)
+result_accuracy= lr_model.evaluate(test_fnl_data)
+result_accuracy.rootMeanSquaredError
+result_accuracy.r2
+fnl_data.describe().show()
