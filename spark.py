f67cb0b8f97413a54e3f6d4e466c757b1dd230eb
nikhilkumar110k <nikhil4040k@gmail.com>
2024-08-29 20:27:58 +0530

    updated code

--- spark.py
+++ spark.py
@@ -23,10 +23,8 @@ df0_10000 = df.filter((col("Prices") < 10000))
 df0_10000.dropDuplicates(["Mobile Name", "Prices", "Reviews", "RAM Specifications", "Storage Specifications"])
 df0_10000.count()
 
-df10000_20000 = df.filter((col("Prices") >= 10000) & (col("Prices") < 20000))
-df10000_20000.dropDuplicates(["Mobile Name", "Prices", "Reviews", "RAM Specifications", "Storage Specifications"])
-df10000_20000.count()
 
-df20000_40000 = df.filter((col("Prices") >= 20000) & (col("Prices") < 40000))
-df20000_40000.dropDuplicates(["Mobile Name", "Prices", "Reviews", "RAM Specifications", "Storage Specifications"])
-df20000_40000.count()
+
+df10000_40000 = df.filter((col("Prices") >= 10000) & (col("Prices") < 40000))
+df10000_40000.dropDuplicates(["Mobile Name", "Prices", "Reviews", "RAM Specifications", "Storage Specifications"])
+df10000_40000.count()
