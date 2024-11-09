5f23299ecbc0d24f59bd2312c40bca3bceff6334
nikhilkumar110k <nikhil4040k@gmail.com>
2024-11-09 16:49:27 +0530

    code pushing

--- sparkstreaming.py
+++ sparkstreaming.py
@@ -3,11 +3,11 @@ import tweepy
 from tweepy import OAuthHandler,StreamingClient,StreamResponse,streaming, OAuth2BearerHandler
 import json
 
-consumer_key='WTMzaTNPX05kVDVGNUxocTJDTC06MTpjaQ'
-consumer_secret='BOdmgMH4wWNnzig19G02BxoP7UIIBJ_fo7SCjvc3oWeblI6azk'
-access_token='1774429511393882112-crl3vLbYurQUTy4Qsv2yjjoPu8jg9x'
-access_secret='YgWUaXGDmahZV8uoIRenZVnzAw3MzKLbpXuiqgCTLrFf0'
-BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAOpwwwEAAAAABvUinglAFPJ5889RnnyDa9m9PCU%3DLeswCkysTDRXvRpMPo7uIBgjFrUARPnXNejXTi1bLg0JwIpQAj'  
+consumer_key=''
+consumer_secret=''
+access_token=''
+access_secret='Y'
+BEARER_TOKEN = 'AAAAAAAAAAj'  
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
@@ -38,9 +38,6 @@ def sendData(c_socket):
   auth=OAuthHandler(consumer_key,consumer_secret)
   auth.set_access_token(access_token,access_secret)
   listener = TweetListener(c_socket, BEARER_TOKEN)
-  existing_rules = listener.get_rules().data
-  if existing_rules:
-      listener.delete_rules([rule.id for rule in existing_rules])
   listener.add_rules(tweepy.StreamRule("Python"))
   listener.filter()
 
